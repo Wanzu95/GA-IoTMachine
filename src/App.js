0d817ab16464744ed9dac406a5ecfd73f88bb37e
Wanzu95 <48009238+Wanzu95@users.noreply.github.com>
2023-02-27 12:21:21 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './comp/Home.js'
+import Temp from './comp/temporary.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
